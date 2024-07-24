73a996f879a5287aa89e6a261e642e1dc7203525
Kevin Payne <payneks@gmail.com>
2024-07-24 06:11:38 -0600

    remove debugging lines

--- internal/igate/igate.go
+++ internal/igate/igate.go
@@ -236,11 +236,7 @@ func (i *IGate) listenForAprsMessages() {
 				continue
 			}
 
-			fmt.Printf("APRS packet type: %v\n", packet.Type().String())
-			fmt.Printf("ForwardToAprsIs: %v\n", packet.Type().ForwardToAprsIs())
-			fmt.Printf("IsAckMessage: %v\n", packet.IsAckMessage())
 			if !packet.IsAckMessage() && packet.Type().ForwardToAprsIs() {
-				fmt.Println("It should be sending the packet to APRS-IS")
 				err = i.Aprsis.Upload(msg)
 				if err != nil {
 					i.Logger.Error("Error uploading APRS packet: ", err)
